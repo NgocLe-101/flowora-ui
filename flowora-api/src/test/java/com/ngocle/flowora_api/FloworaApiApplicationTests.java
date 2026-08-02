8584c847ffe659532ab5bd78d16a51146309f293
Ngoc Le <ngocle266tqt@gmail.com>
2026-08-02 15:23:16 +0700

    feat: init backend repo

---
+++ flowora-api/src/test/java/com/ngocle/flowora_api/FloworaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngocle.flowora_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloworaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
